9f6f34441917040603ad7a4957872757d455f287
sofiamaciel212 <sofiamaciel2112@gmail.com>
2023-11-30 14:19:50 -0300

    agregue interacciones
    agregue interacciones con los botones

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,40 +1,53 @@
+let sectionSeleccionarAtaque = document.getElementById("seleccionar-ataque")
+let sectionReiniciar = document.getElementById("reiniciar")
+let botonMascotaJugador = document.getElementById("boton-mascota")
+let botonFuego = document.getElementById("boton-fuego")
+let botonAgua = document.getElementById("boton-agua")
+let botonTierra = document.getElementById("boton-tierra")
+sectionReiniciar.style.display = "none"
+let botonReiniciar = document.getElementById("boton-reiniciar")
+
+let sectionSeleccionarMascota = document.getElementById("seleccionar-mascota")
+let sectionSeleccionarAtaque = document.getElementById("seleccionar-ataque")
+let inputHipodoge = document.getElementById("hipodoge")
+let inputCapipepo = document.getElementById("capipepo")
+let inputRatigueya = document.getElementById("ratigueya")
+let spanMascotaJugador = document.getElementById ("mascota-jugador")
+
 let ataqueJugador
 let ataqueEnemigo
 let vidasJugador = 3
 let vidasEnemigo = 3
 
 function iniciarJuego() {
-    let sectionSeleccionarAtaque = document.getElementById("seleccionar-ataque")
+   
     sectionSeleccionarAtaque.style.display = "none"
 
-    let sectionReiniciar = document.getElementById("reiniciar")
-    sectionReiniciar.style.display = "none"
+   
+ 
 
-    let botonMascotaJugador = document.getElementById("boton-mascota")
+  
     botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
 
-    let botonFuego = document.getElementById("boton-fuego")
+   
     botonFuego.addEventListener("click", ataqueFuego)
-    let botonAgua = document.getElementById("boton-agua")
+   
     botonAgua.addEventListener("click", ataqueAgua)
-    let botonTierra = document.getElementById("boton-tierra")
+   
     botonTierra.addEventListener("click", ataqueTierra)
 
-    let botonReiniciar = document.getElementById("boton-reiniciar")
+   
     botonReiniciar.addEventListener("click", reiniciarJuego)
 }
 
 function seleccionarMascotaJugador() {
-    let sectionSeleccionarMascota = document.getElementById("seleccionar-mascota")
+   
     sectionSeleccionarMascota.style.display = "none"
 
-    let sectionSeleccionarAtaque = document.getElementById("seleccionar-ataque")
+  
     sectionSeleccionarAtaque.style.display = "flex"
 
-    let inputHipodoge = document.getElementById("hipodoge")
-    let inputCapipepo = document.getElementById("capipepo")
-    let inputRatigueya = document.getElementById("ratigueya")
-    let spanMascotaJugador = document.getElementById ("mascota-jugador")
+   
 
     if (inputHipodoge.checked) {
         spanMascotaJugador.innerHTML = "Hipodoge"
@@ -157,7 +170,6 @@ function crearMensajeFinal(resultadoFinal) {
     sectionReiniciar.style.display = "block"
 }
 
-
 function reiniciarJuego() {
     location.reload()
 }
@@ -165,6 +177,5 @@ function reiniciarJuego() {
 function aleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
-
 
 window.addEventListener("load", iniciarJuego)    
